218a9479da32f9c54d5a4c58dbbc68e584a06ccf
travis <trng2200@gmail.com>
2024-05-13 01:15:55 -0700

    implement crud keyvalue store

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type key struct {
+	Value string `json:"value"`
+}
+
+var keys = map[string]string{}
 
 func getPing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "I'm alive!!"})
@@ -19,8 +24,53 @@ func main() {
 	router.GET("/ping/:name", getPingByName)
 	router.POST("/echo", postEcho)
 	router.GET("/echo", getEcho)
+	router.PUT("/key-value-store/:key", postValue)
+	router.GET("/key-value-store/:key", getValue)
+	router.DELETE("/key-value-store/:key", deleteValue)
 
     router.Run(":8085")
+}
+
+func deleteValue(c *gin.Context) {
+	key := c.Param("key")
+
+	if _, exists := keys[key]; !exists {
+		c.JSON(http.StatusNotFound, gin.H{"doesExist": false, "error": "Key does not exist", "message": "Error in DELETE"})
+	} else {
+		delete(keys, key)
+		c.JSON(http.StatusOK, gin.H{"doesExist": true, "message": "Deleted successfully"})
+	}
+}
+
+func getValue(c *gin.Context) {
+	key := c.Param("key")
+
+	if _, exists := keys[key]; !exists {
+		c.JSON(http.StatusNotFound, gin.H{"doesExist": false, "error": "Key does not exist", "message": "Error in GET"})
+	} else {
+		value := keys[key]
+		c.JSON(http.StatusOK, gin.H{"doesExist": true, "message": "Retrieved successfully", "value": value})
+	}
+}
+
+func postValue(c *gin.Context) {
+	var newKey key
+	param := c.Param("key")
+
+    if err := c.BindJSON(&newKey); err != nil {
+        return
+    } else if len(param) > 50 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Key is too long", "message": "Error in PUT"})
+	} else if newKey.Value == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Value is missing", "message": "Error in PUT"})
+	} else if _, exists := keys[param]; exists {
+		keys[param] = newKey.Value
+		c.JSON(http.StatusOK, gin.H{"message": "Updated successfully", "replaced": true})
+	} else {
+		keys[param] = newKey.Value
+		c.JSON(http.StatusCreated, gin.H{"message": "Added successfully", "replaced": false})
+	}
+    
 }
 
 func getEcho(c *gin.Context) {
